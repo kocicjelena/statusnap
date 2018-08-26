1b17af10de55d4809a72d575c735d62a8dd3f434
Jelena Kocic <kocicjelena@gmail.com>
2018-08-26 18:41:49 +0200

    next: test setup

--- setup.py
+++ setup.py
@@ -1,1 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
 
+from setuptools import setup
+
+
+package_name = 'status'
+filename = package_name + '.py'
+
+
+def get_version():
+    import ast
+
+    with open(filename) as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
+
+
+def get_long_description():
+    try:
+        with open('README.md', 'r') as f:
+            return f.read()
+    except IOError:
+        return ''
+
+
+setup(
+    name=package_name,
+    version=get_version(),
+    author='kocicjelena',
+    author_email='kocicjelena@gmail.com',
+    description='snap py made simple',
+    url='https://github.com/kocicjelena/statusnap',
+    long_description=get_long_description(),
+    py_modules=[package_name],
+    entry_points={
+        'console_scripts': [
+            'status = status:main'
+        ]
+    },
+)
